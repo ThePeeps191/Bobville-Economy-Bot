9fab73d94753544a4043ce321ced919ac150c2d9
The Peeps191 <74725787+ThePeeps191@users.noreply.github.com>
2021-01-06 13:23:48 -0700

    Create trivia.py

---
+++ Bobville Economy Bot Code/trivia.py
@@ -0,0 +1,31 @@
+import random
+
+easy = {}
+easy_list = []
+for key in easy:
+	easy_list.append("key")
+
+medium = {}
+medium_list = []
+for key in medium:
+	medium_list.append("key")
+
+hard_list = []
+hard = {}
+for key in hard:
+	hard_list.append("key")
+
+
+class MainTrivia:
+	def __init__(self):
+		self.easy = easy
+		self.medium = medium
+		self.hard = hard
+		self.easys = easy_list
+		self.mediums = medium_list
+		self.hards = hard_list
+	def easy(self):
+		self.question
+
+
+Trivia = MainTrivia()
