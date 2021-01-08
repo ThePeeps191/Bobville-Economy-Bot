b43c2e894c0917a632b31791411cfb39b08706b6
The Peeps191 <74725787+ThePeeps191@users.noreply.github.com>
2021-01-08 10:18:25 -0700

    Added HelpSubcommand Template

--- Bobville Economy Bot Code/bot_data.py
+++ Bobville Economy Bot Code/bot_data.py
@@ -1,3 +1,13 @@
 '''
 DATA FOR THE BOT
 '''
+
+# Import Discord
+import discord
+
+# Basic HelpSubcommand
+@help.command(aliases=["p", "pro"])
+async def profile(ctx):
+	em = discord.Embed(title="Profile Command", description="Checks Your Profile", color=ctx.author.color)
+	em.add_field(name="INFO", value="**Description**:\nChecks Your Game Profile\n\n**Syntax/Usage**:\n`?profile <optional member (mention them)>`\n\n**Cooldown**:\nNone\n\n**Aliases**:\n`p`, `pro`")
+	await ctx.send(embed=em)
